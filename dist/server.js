76494a66bd9ac35f9f395f5878ce635025401267
vamsigajula14 <gajulavamsi90@gmail.com>
2025-03-23 14:17:16 +0530

    'second'

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Paddy Cultivation Platform API is running!");
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
